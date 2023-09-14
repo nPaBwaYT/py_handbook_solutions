0bf3add2117b1a836f4a554077339d177f61f89d
nPaBwaYT <surge.anton@gmail.com>
2023-09-14 08:06:49 +0300

    L

---
+++ solutions/2_2.1_L.py
@@ -0,0 +1,22 @@
+def solution():
+    a = str(input())
+    b = str(input())
+    res = ''
+
+    for i in range(-1, -(min(len(a), len(b))) - 1, -1):
+        res += str((int(a[i]) + int(b[i])) % 10)
+
+    ost = len(a) - len(b)
+    if ost >= 0:
+        res += a[:ost:][::-1]
+    else:
+        res += b[:abs(ost):][::-1]
+    print(int(res[::-1]))
+
+
+def main():
+    solution()
+
+
+if __name__ == "__main__":
+    main()
